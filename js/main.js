582e2b8014ef3a19cea608b7112943d3d68ac22b
Agustín E Santillán <agustinsantillanog@gmail.com>
2024-04-30 12:02:18 -0300

    Añadiendo bootstrap

--- js/main.js
+++ js/main.js
@@ -125,3 +125,20 @@ const showHTML = () => {
 	valorTotal.innerText = `$${total}`;
 	countProducts.innerText = totalOfProducts;
 };
+
+
+// Login
+
+const username = document.getElementById('username')
+const password = document.getElementById('password')
+const button = document.getElementById('button')
+
+button.addEventListener('click', (e) => {
+    e.preventDefault()
+    const data = {
+        username: username.value,
+        password: password.value
+    }
+
+    console.log(data)
+})
